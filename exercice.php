552ce158d5358d012f7b459ac4080ba2105350fc
Emilie VINCENT-SULLY <emivsy@gmail.com>
2025-05-22 15:48:56 +0200

    step 1 done, starting 2

---
+++ exercice.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+            
+            if(isset($_GET['first_name']) &&$_GET['first_name'] !== '' ) {
+                echo "Bonjour, " .$_GET['first_name'];
+            }
+            else{
+                echo "Bonjour, Anonyme";
+            }
+            ?>
+
+
+<form action="exercice.php" method="post">
+ <p>Votre nom : <input type="text" name="first_name" /></p>
+ <p><input type="submit" value="OK"></p>
+</form>
+
+    
+</body>
+</html>
